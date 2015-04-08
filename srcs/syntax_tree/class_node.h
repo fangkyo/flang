eafe063afde580898a55c3cc38a568a09abce0e7
fangkyo <fangkyo@gmail.com>
2015-04-07 21:51:01 -0700

    format class_node

--- srcs/syntax_tree/class_node.h
+++ srcs/syntax_tree/class_node.h
@@ -15,72 +15,71 @@ using namespace std;
 
 class ErrorEngine;
 
+class ClassNode : public SyntaxNode {
 
-class ClassNode : public SyntaxNode{
+private:
+  string m_name;       // class name
+  string m_parentName; // parent class name
+  vector<DeclareNode *> m_varList; // 注意 : 一个declare可能声明多个成员变量，
+  // 因此这个varList不能简单的看作成员变量列表
+  vector<ClassFuncNode *> m_funcList; // function members list
 
-    private:
-        string m_name;                    // class name
-        string m_parentName;              // parent class name
-        vector<DeclareNode*>  m_varList;  // 注意 : 一个declare可能声明多个成员变量，
-                                          // 因此这个varList不能简单的看作成员变量列表
-        vector<ClassFuncNode*>     m_funcList; // function members list
+  ClassNode *m_parent;
 
-        ClassNode* m_parent;
+public:
+  ClassNode() : m_parent(NULL) {}
+  ~ClassNode();
 
-    public:
-        ClassNode() : m_parent(NULL) {  }
-        ~ClassNode();
+  void setName(const string &name) { m_name = name; }
+  const string &getName() { return m_name; }
 
-        void setName( const string& name ) {  m_name = name; }
-        const string& getName() {  return m_name; }
+  void setParentName(const string &name) { m_parentName = name; }
+  const string &getParentName() { return m_parentName; }
 
-        void setParentName( const string& name ) { m_parentName = name; }
-        const string& getParentName() { return m_parentName; }
+  void setParent(ClassNode *parent);
+  ClassNode *getParent() { return m_parent; }
+  int getVarsNum() { return m_varList.size(); }
+  int getFuncsNum() { return m_funcList.size(); }
 
-        void setParent( ClassNode* parent );
-        ClassNode* getParent() { return m_parent; }
-        int getVarsNum() { return m_varList.size(); }
-        int getFuncsNum() { return m_funcList.size(); }
+  bool hasParent() { return m_parentName.length() > 0; }
 
-        bool hasParent() {  return m_parentName.length() > 0; }
+  void addVarDeclare(DeclareNode *declare);
 
-        void addVarDeclare( DeclareNode* declare );
+  void addFuncion(ClassFuncNode *func);
+  void accept(Visitor &visitor);
 
-        void addFuncion( ClassFuncNode* func );
-        void accept( Visitor& visitor );
+  void acceptVars(Visitor &visitor);
 
-        void acceptVars( Visitor& visitor );
+  void acceptFuncs(Visitor &visitor, ScopeManager &scopeManager,
+                   ErrorEngine &errorEngine);
 
-        void acceptFuncs(Visitor& visitor, ScopeManager& scopeManager, ErrorEngine& errorEngine);
+  string toString() {
+    string str("class ");
+    str.append(m_name);
+    return str;
+  }
 
-        string toString() {
-            string str( "class " );
-            str.append( m_name );
-            return str;
-        }
+  ClassFuncNode *findMemberFunc(const string &funcName,
+                                vector<DataTypeNode *> &paramsType);
 
-        ClassFuncNode* findMemberFunc( const string& funcName,
-                                       vector<DataTypeNode*>& paramsType );
+  ClassFuncNode *findMemberFunc(const string &funcName);
 
-        ClassFuncNode* findMemberFunc( const string& funcName );
+  VarNode *findMemberVar(const string &varName);
 
-        VarNode*  findMemberVar( const string& varName );
-
-        ClassNode* getClassNode() { return this; }
+  ClassNode *getClassNode() { return this; }
 };
 
 /*!
  * \brief MemberVarRefNode is the reference of a class member.
  */
 class MemberVarRefNode : public ExpNode {
- public:
-  MemberVarRefNode(const std::string& var_name);
+public:
+  MemberVarRefNode(const std::string &var_name);
   virtual ~MemberVarRefNode() {}
-  void setOwner(ClassNode* owner) {
-    owner_ = owner;
-  }
- private:
-  ClassNode* owner_; //< The owner class of this member variable.
+  void setOwner(ClassNode *owner) { owner_ = owner; }
+
+private:
+  ClassNode *owner_; //< The owner class of this member variable.
   std::string var_name_;
 };
 
